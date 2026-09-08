4f814359f71c152205219dd75e365089484be5b1
Brayan Camargo <camilo.un.estudio77@gmail.com>
2026-09-07 19:40:30 -0500

    Se actualizaron estas clases

--- src/Modelo/Jugador.py
+++ src/Modelo/Jugador.py
@@ -1,2 +1,11 @@
+from .mano import Mano
+
+
 class Jugador():
-    pass
+    def __init__(self,nombre):
+
+        self.nombre = nombre
+
+    def jugador_mano(self,baraja_actual):
+        mano1 = Mano()
+        self.mano = mano1.obtener_mano(baraja_actual)
