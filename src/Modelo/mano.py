1444c582159f9b852f2a2e33b06fdd8dc0ab4a2b
Brayan Camargo <camilo.un.estudio77@gmail.com>
2026-09-11 21:52:16 -0500

    Se añadio print y se borraron lines inecesarias

--- src/Modelo/mano.py
+++ src/Modelo/mano.py
@@ -3,8 +3,8 @@ from .baraja import Baraja
 class Mano():
 
     def obtener_mano(self,bara_actual):
+        print("MANO:")
         posiciones = bara_actual.obtener_carta(2)
-        print(posiciones[0])
-        print(posiciones[1])
         self.mano = [bara_actual.baraja[posiciones[0]], bara_actual.baraja[posiciones[1]]]
         bara_actual.borrar_carta(posiciones)
+
